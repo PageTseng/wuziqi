7c1a1d947c7c8fd8519ef56af62971ae4be89f64
PIGGY0204 <L_P_0204@outlook.com>
2017-08-18 10:44:16 +0800

    merge Point.java into PointArrayList.java

--- src/PointArrayList.java
+++ src/PointArrayList.java
@@ -20,4 +20,31 @@ public class PointArrayList {
     //todo
     public boolean check(Point point) {
     }
+
+    public class Point {
+        private int x, y;
+        private char color;
+
+        public Point(int x, int y, char color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+
+        //todo
+        public boolean equals(Point point) {
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public char getColor() {
+            return color;
+        }
+    }
 }
